ae78a89499a24c01a4a463c25b6372cba1470bab
Scheyk <kathrin.dostal@gmail.com>
2020-10-13 16:12:09 +0200

    show the hours message

--- kathi/task1/script.js
+++ kathi/task1/script.js
@@ -1,27 +1,34 @@
 let today = new Date().getDay();
+let hours = new Date().getHours();
+let message;
+if (hours < 15) {
+    message = "not time to drink yet";
+} else {
+    message = "yes, it's time to rock";
+}
 console.log(today);
 
 switch (today) {
     case 1:
-        document.getElementById('result').innerHTML = "monday";
+        document.getElementById('result').innerHTML = "monday " + message;
         break;
     case 2:
-        document.getElementById('result').innerHTML = "tuesday";
+        document.getElementById('result').innerHTML = "tuesday " + message;
         break;
     case 3:
-        document.getElementById('result').innerHTML = "wednesday";
+        document.getElementById('result').innerHTML = "wednesday " + message;
         break;
     case 4:
-        document.getElementById('result').innerHTML = "thursday";
+        document.getElementById('result').innerHTML = "thursday " + message;
         break;
     case 5:
-        document.getElementById('result').innerHTML = "friday";
+        document.getElementById('result').innerHTML = "friday " + message;
         break;
     case 6:
-        document.getElementById('result').innerHTML = "saturday";
+        document.getElementById('result').innerHTML = "saturday " + message;
         break;
-    case 7:
-        document.getElementById('result').innerHTML = "sunday";
+    case 0:
+        document.getElementById('result').innerHTML = "sunday " + message;
         break;
     default:
         document.getElementById('result').innerHTML = "no match";
