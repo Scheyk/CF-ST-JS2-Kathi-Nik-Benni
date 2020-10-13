b358598368d05dda3edfd43eb1df417cd69b54f4
Scheyk <kathrin.dostal@gmail.com>
2020-10-13 16:13:00 +0200

    showing random temp

--- kathi/task4/script.js
+++ kathi/task4/script.js
@@ -3,15 +3,15 @@
 let randomTemp = Math.floor(Math.random() * 45) - 5;
 
 if (randomTemp <= 10) {
-    document.getElementById('result').innerHTML = "<img src='../img/cold.png' alt='img cold' width='150px'> <p class='h3 mt-4'> The weather is cold </p>";
+    document.getElementById('result').innerHTML = randomTemp + "<img src='../img/cold.png' alt='img cold' width='150px'> <p class='h3 mt-4'> The weather is cold </p>";
 } else {
     if (randomTemp <= 20) {
-        document.getElementById('result').innerHTML = "<img src='../img/ok.png' alt='img cold' width='150px'> <p class='h3 mt-4'> The weather is ok</p>";
+        document.getElementById('result').innerHTML = randomTemp + "<img src='../img/ok.png' alt='img cold' width='150px'> <p class='h3 mt-4'> The weather is ok</p>";
     } else {
         if (randomTemp <= 30) {
-            document.getElementById('result').innerHTML = "<img src='../img/warm.png' alt='img cold' width='150px'> <p class='h3 mt-4'> The weather is warm</p>";
+            document.getElementById('result').innerHTML = randomTemp + "<img src='../img/warm.png' alt='img cold' width='150px'> <p class='h3 mt-4'> The weather is warm</p>";
         } else {
-            document.getElementById('result').innerHTML = "<img src='../img/hot.png' alt='img cold' width='150px'> <p class='h3 mt-4'> The weather is hot</p>";
+            document.getElementById('result').innerHTML = randomTemp + "<img src='../img/hot.png' alt='img cold' width='150px'> <p class='h3 mt-4'> The weather is hot</p>";
         }
     }
 }
